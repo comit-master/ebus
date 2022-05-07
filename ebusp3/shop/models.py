caf1f9b499466fd1da313b617ec2aa3659040d40
ranemr <ranemriad@gmail.com>
2022-05-07 13:55:49 +0200

    admin and models classes updates

--- ebusp3/shop/models.py
+++ ebusp3/shop/models.py
@@ -1,3 +1,28 @@
+from unicodedata import category
 from django.db import models
-
+from django.db.models.fields.related import ForeignKey
 # Create your models here.
+
+
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-date_added']
+    #section added in first line
+
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    description = models.CharField(max_length=200)
+    categorie = ForeignKey(Section, related_name='section', on_delete=models.CASCADE)
+    image = models.CharField(max_length=5000)
+    date_added = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-date_added']
