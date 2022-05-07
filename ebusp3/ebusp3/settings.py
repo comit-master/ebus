ca57723f0e0a14dd55e529ac4678e68ac40765a6
ranemr <ranemriad@gmail.com>
2022-05-07 13:42:01 +0200

    update setting and push develop branch

--- ebusp3/ebusp3/settings.py
+++ ebusp3/ebusp3/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates')
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for production #dev
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
