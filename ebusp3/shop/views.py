7885d727062d061a90cd00eb24afdfe167f16d05
ranemr <ranemriad@gmail.com>
2022-05-08 16:19:59 +0200

    page block added

--- ebusp3/shop/views.py
+++ ebusp3/shop/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .models import Product
-
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 
 def displayhome(request):
     product_object = Product.objects.all() #retrieve all object from db
+    item_name = request.GET.get('item-name')
+    if item_name !='' and item_name is not None: # item-name is the object from index.html
+        product_object = Product.objects.filter(title__icontains=item_name) #when user is writing if in db in title
+        # 1 caracter of what the user is writing, display only the product 
+        # title__icontains similar to like sql fct
+    paginator = Paginator(product_object, 4) # show me only 4 products by page
+    page = request.GET.get('page') # display the page index number 
+    product_object = paginator.get_page(page) # get the page refer to the index number
     return render(request, 'shop/index.html', {'product_object': product_object})
